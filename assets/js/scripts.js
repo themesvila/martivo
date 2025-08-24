9a671159b80fa9c1da34961bb7424ebdfef823da
Masum Billah <bograhost@gmail.com>
2025-08-24 16:13:19 +0600

    ''

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -55,9 +55,9 @@
 		});
 
 		/*Counter*/
-		$('.counter_up').on('inview', function(event, visible, visiblePartX, visiblePartY) {
+		$('.counterup').on('inview', function(event, visible, visiblePartX, visiblePartY) {
 			if (visible) {
-				$(this).find('span.count').each(function () {
+				$(this).find('span.counter').each(function () {
 					var $this = $(this);
 					$({ Counter: 0 }).animate({ Counter: $this.text() }, {
 						duration: 2000,
@@ -115,59 +115,38 @@
 	});
 	
 
-	// Testimonials
-	var testthumb = new Swiper(".test_thumb", {
+ // Student Images (thumbnails)
+	var testimonialImages = new Swiper('.testimonial_images', {
 		loop: true,
-		spaceBetween: 0,
-		slidesPerView: 1,
-		effect: 'fade',
-		freeMode: true,
-		watchSlidesProgress: true,
-		loop: true,
-	});
-
-	var test_slider = new Swiper('.test_slider', {
-		// Optional parameters
-		slidesPerView: 1,
+		slidesPerView: 5,
 		spaceBetween: 30,
-		effect: 'slide',
-		loop: true,
-		// Navigation arrows
-		navigation: {
-			nextEl: '.tarrow_right',
-			prevEl: '.tarrow_left',		
-		},
-
-		breakpoints: {
-			1299: {
-				slidesPerView: 1,
-			},
-			1199: {
-				slidesPerView: 1,
-			},					
-			1024: {
-				slidesPerView: 1,
-			},
-			991: {
-				slidesPerView: 1,
-			},			
-
-			767: {
-				slidesPerView: 1,
-			},
-			575: {
-				slidesPerView: 1,
-			},
-			0: {
-				slidesPerView: 1,
-			},
-		},
-
-		thumbs: {
-			swiper: testthumb,
-		},
+		centeredSlides: true,
+		slideToClickedSlide: true
 	});
 
+  // Student Testimonials (text)
+  var testimonialSlider = new Swiper('.testimonial_slider', {
+    loop: true,
+    autoHeight: true,
+    pagination: {
+      el: '.testimonial-pagination',
+      clickable: true,
+    },
+  });
+
+  // Sync both sliders
+  testimonialImages.controller.control = testimonialSlider;
+  testimonialSlider.controller.control = testimonialImages;
+
+  // Add active class to center image
+  function updateActiveImage(){
+    $('.testimonial_images .swiper-slide').removeClass('active');
+    $('.testimonial_images .swiper-slide.swiper-slide-active').addClass('active');
+  }
+
+  testimonialImages.on('init', updateActiveImage);
+  testimonialImages.on('slideChangeTransitionEnd', updateActiveImage);
+  testimonialImages.init();
 
 	//Clients
 	$('.clients_slider').owlCarousel({
@@ -194,6 +173,40 @@
 				items: 6
 			}
 		}
+	});
+
+	//------------- Event Slider -------------//
+
+	var eventSwiper = new Swiper(".event_slider", {
+		loop: true,
+		centeredSlides: true,
+		slidesPerView: 3,
+		spaceBetween: 20,
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+	});
+
+	//------------- Client Slider -------------//
+	var clientSwiper = new Swiper(".client_slider", {
+		loop: true,
+		slidesPerView: 5,       // number of logos to show
+		spaceBetween: 20,       // gap between slides
+		autoplay: {
+			delay: 2500,
+			disableOnInteraction: false,
+		},
+		breakpoints: {
+			320: { slidesPerView: 2 },
+			640: { slidesPerView: 3 },
+			1024: { slidesPerView: 5 },
+		},
+
 	});
 
 	//------------- DETAIL ADD - MINUS COUNT ORDER -------------//
